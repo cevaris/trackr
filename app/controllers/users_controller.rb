fe1d3c488f422eabc6d5ae5d7f5d66256de61b27
Cevaris <cevaris@gmail.com>
2014-05-28 15:38:46 -0600

    fixed devise and user api clash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,14 @@ class UsersController < ApplicationController
   private
   
   def restrict_access
-    Rails.logger.info "Current User #{current_user}"
-    @user ||= User.find_by_id_and_auth_token params[:id]||'', request.headers['Auth-Token']
-    Rails.logger.info "Post Current User #{current_user}"
-    # api_key = ApiKey.find_by_access_token(params[:access_token])
-    head :unauthorized unless @user
+    unless current_user
+      Rails.logger.info "Current User #{current_user}"
+      @user ||= User.find_by_id_and_auth_token params[:id]||'', request.headers['Auth-Token']
+      Rails.logger.info "Post Current User #{current_user}"
+      head :unauthorized unless @user
+    else
+      @user = current_user
+    end
   end
 
 end
