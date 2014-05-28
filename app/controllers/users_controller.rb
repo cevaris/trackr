e4820e2deb2324736e3d8cd8355bcd00a36c9a34
Cevaris <cevaris@gmail.com>
2014-05-28 15:37:11 -0600

    working on api collision vs users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,23 @@
 class UsersController < ApplicationController
 
-  # def current_user
-  #   # puts params
-  #   # puts request.headers
-  #   current_user ||= User.find_by_id_and_auth_token params[:id], request.headers['Auth-Token'] if params.has_key?(:id)
-  # end
+  before_filter :restrict_access
+
 
   def show
-    current_user ||= User.find_by_id_and_auth_token params[:id], request.headers['Auth-Token'] if params.has_key?(:id)
-
     respond_to do |format|
-      format.json { render json: current_user.to_json(:only => [:id, :private_key]) }
+      format.json { render json: @user.to_json(:only => [:id, :private_key]) }
       format.html
     end
   end
+
+  private
+  
+  def restrict_access
+    Rails.logger.info "Current User #{current_user}"
+    @user ||= User.find_by_id_and_auth_token params[:id]||'', request.headers['Auth-Token']
+    Rails.logger.info "Post Current User #{current_user}"
+    # api_key = ApiKey.find_by_access_token(params[:access_token])
+    head :unauthorized unless @user
+  end
+
 end
